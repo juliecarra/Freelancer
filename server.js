d12432aae2e4e37b31ba39b25f744679d378ad65
Julie Carra <julie.carra@live.fr>
2019-09-30 11:49:49 +0200

    make it better

--- server.js
+++ server.js
@@ -15,24 +15,18 @@ app.use(express.json({ extended: false })); //Get datas from req.body
 
 app.use(cors("http://localhost:3000"));
 
-//app.get("/", (req, res) => res.send("API Running"));
-
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 
 app.use("/api/posts", require("./routes/api/posts"));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 1995;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
